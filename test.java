952f71c6d4cbef14b0b8fdf2d1f041eb428dd9d1
1U23CA029DHAKSHA <139437377+1U23CA029DHAKSHA@users.noreply.github.com>
2024-12-12 09:46:38 +0000

    submit

--- test.java
+++ test.java
@@ -1,19 +1,15 @@
-DSA TEST PROGRAM
-
 import java.util.Scanner;
+ class test{
+    public static void main(String args[]){
+Scanner myObj=new Scanner(System.in);
+System.out.println("Enter name,age and salary:");
 
-class test {
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
 
-    System.out.println("Enter name, age and salary:");
-
-    String name = myObj.nextLine();
-    // 
-       TYPE YOUR CODE HERE
-      //
-    System.out.println("Name: " + name);
-    System.out.println("Age: " + age);
-    System.out.println("Salary: " + salary);
-  }
+String name=myObj.nextLine();
+int age =myObj.nextInt();
+double salary=myObj.nextDouble();
+System.out.println("Name: "+name);
+System.out.println("Age: "+age);
+System.out.println("Salary: "+salary);
+ }
 }
